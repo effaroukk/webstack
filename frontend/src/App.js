1aa09fb98172a111ef517642c581a2cbd4d43a6e
effaroukk <efqay1@gmail.com>
2025-01-21 19:25:26 +0000

    main app component

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Auth from './components/Auth';
+import Home from './pages/Home';
+
+function App() {
+    return (
+        <div>
+            <Auth />
+            <Home />
+        </div>
+    );
+}
+
+export default App;
+
