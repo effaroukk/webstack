90a57c50f4c0e760f20f55a07a89a329561b3337
effaroukk <efqay1@gmail.com>
2025-01-21 19:24:11 +0000

    home page setup

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to Your Digital Diary</h1>
+        </div>
+    );
+};
+
+export default Home;
+
