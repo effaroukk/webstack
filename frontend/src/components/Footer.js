8a6b35bf89b79e4af134a6137ca3a1ef0f5b0604
effaroukk <efqay1@gmail.com>
2025-01-23 10:27:01 +0000

    footer setup

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>© 2025 My Digital Diary. All Rights Reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
+
